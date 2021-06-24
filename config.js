c2bc5bc7d3f19bef4fcce6bee16182ba5566141d
RIFAIGGZ <76506048+rifaiganz@users.noreply.github.com>
2021-06-25 05:14:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['belum jadi']
+global.owner = ['6287754224995','6281220951879','6285877748277']// Put your number here
+global.mods = ['62856438334749','628990911211'] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zeks: 'https://api.zeks.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'JoanB',
+  'https://zeks.xyz': 'Rifaiitugey'
+}
+
+// Sticker WM
+global.packname = 'RIFAI GG'
+global.author = 'AWAS COLONG'
+
+global.multiplier = 100 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
