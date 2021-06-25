bb8df40b453c9d2c0b8583ecbb09556708a64a9f
RIFAIGGZ <76506048+rifaiganz@users.noreply.github.com>
2021-06-25 20:31:10 +0700

    Add files via upload

---
+++ plugins/Spamchat.js
@@ -0,0 +1,32 @@
+let handler = async (m, { conn, args, usedPrefix }) => {
+  let wehr = args.join (' ')
+  let noPrefix = m.text.replace(usedPrefix, '')
+	   let riff = noPrefix.trim().split`|`.slice(1)
+	   for (let i = 0; i < wehr[0]; i++) {
+	     conn.sendMessage(m.chat, `wehr`, m.mtype)
+	   }
+}
+
+handler.help = ['spamchat <text>|<brp>']
+
+	                                    handler.tags = ['owner']
+
+
+	                                    handler.command = /^spamchat$/i
+
+	                                    handler.owner = true
+
+	                                    handler.mods = false
+
+	                                    handler.premium = false
+	                                    handler.group = false
+	                                    handler.private = false
+
+	                                    handler.admin = false
+	                                    handler.botAdmin = false
+
+	                                    handler.fail = null
+
+	                                    module.exports = handler
+
+	            
