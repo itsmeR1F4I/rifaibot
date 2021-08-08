85490982850d41219705c41e12da28d1f026beed
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2021-08-08 14:40:40 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   // this.fakeReply(m.chat, 'This is my owner', '6281390658325@s.whatsapp.net', 'Owner')
   this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281390658325', 'Owner Metro Bot :)', m)
+  this.sendContact(m.chat, '6287754224995', 'Owner RIFAIBOT', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
