e758c50a5b9205c1220b344f09de79b486920d43
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2021-11-03 12:40:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -227,6 +227,29 @@ async function _quickTest() {
   require('./lib/sticker').support = s
   Object.freeze(global.support)
 
+let uptime = process.uptime()
+function count(seconds){
+
+if (typeof seconds !== "number") throw "connError: Unexpected Param " + typeof seconds
+
+let hours = Math.floor(seconds / (60*60));
+let minutes = Math.floor(seconds % (60*60) / 60);
+let second = Math.floor(seconds % 60);
+return `${pad(hours)} Jam ${pad(minutes)} Menit ${pad(second)} Detik`
+}
+function pad(s) {
+return (s < 10 ? '0' : '') + s;
+}
+
+
+let settingstatus = 0;
+    if (new Date() * 1 - settingstatus > 1000) {
+      let _uptime = process.uptime() * 1000;
+
+await conn.setStatus(`Aktif Selama : ${count(uptime)}`)
+settingstatus = new Date() * 1;            
+          }
+
   if (!s.ffmpeg) conn.logger.warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
   if (s.ffmpeg && !s.ffmpegWebp) conn.logger.warn('Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
   if (!s.convert && !s.magick && !s.gm) conn.logger.warn('Stickers may not work without imagemagick if libwebp on ffmpeg doesnt isntalled (pkg install imagemagick)')
